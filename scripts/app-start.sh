1d505535e0486e14e859ac048abdb2efd13e58f3
Jakub Mucha <jakub.mucha@icloud.com>
2022-12-21 22:33:10 +0100

    chore: update scripts
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 home_path=/home/ubuntu
-bin_path=$home_path/bin
 docker_path=$home_path/.docker/cli-plugins
 
 export COMPOSE_HTTP_TIMEOUT=200
 
 cd $home_path/snx-api &&
-	$bin_path/docker volume prune --force &&
 	$docker_path/docker-compose up -d
---
+++ scripts/app-stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+home_path=/home/ubuntu
+bin_path=/usr/bin
+docker_path=$home_path/.docker/cli-plugins
+
+cd $home_path/snx-api &&
+	$docker_path/docker-compose down --volumes --rmi all &&
+	$bin_path/docker system prune --all --volumes --force
