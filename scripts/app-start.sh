2dbeea13e1f6166734a108f478f1996ef602a80b
Jakub Mucha <jakub.mucha@icloud.com>
2022-12-22 22:56:25 +0100

    feat: use aws-env
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -2,7 +2,5 @@
 home_path=/home/ubuntu
 docker_path=$home_path/.docker/cli-plugins
 
-export COMPOSE_HTTP_TIMEOUT=200
-
 cd $home_path/snx-api &&
-	$docker_path/docker-compose up -d
+	COMPOSE_HTTP_TIMEOUT=200 $docker_path/docker-compose up -d
