8f283d0e66b27bab4d92e742111caf81f1c10699
drptbl <jakub.mucha@icloud.com>
2024-04-20 16:48:49 +0200

    chore: wl snx.eth.limo
    Signed-off-by: drptbl <jakub.mucha@icloud.com>

--- nginx/scripts/setup-whitelist.sh
+++ nginx/scripts/setup-whitelist.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # whitelist domains
 printf '"~*(?:\\b)synthetix\.io(?:\\b)" 			0;\n' >>/etc/nginx/bots.d/whitelist-domains.conf
+printf '"~*(?:\\b)synthetix\.eth\.limo(?:\\b)" 			0;\n' >>/etc/nginx/bots.d/whitelist-domains.conf
