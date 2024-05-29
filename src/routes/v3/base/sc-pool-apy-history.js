fca45e873943f3949a8a3f6009115c7b2df44dd3
fritzschoff <fritzschoff@icloud.com>
2024-05-29 15:25:28 +0100

    feat: added new route for history apr

--- src/routes/v3/base/sc-pool-apy-history.js
+++ src/routes/v3/base/sc-pool-apy-history.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { log, postgresClient, getCache, setCache } = require('../../../utils');
 
-const cacheKey = 'sc-pool-apy';
+const cacheKey = 'sc-pool-apy-history';
 
 fetchDataFromPostgres();
 const cacheTime =
@@ -153,7 +153,7 @@ router.get('/', async (req, res, next) => {
       res.json(responseData);
     }
   } catch (error) {
-    log.error(`[v3BaseSCPoolAPY] Error: ${error.message}`);
+    log.error(`[v3BaseSCPoolAPYHistory] Error: ${error.message}`);
     next(error);
   }
 });
@@ -161,117 +161,122 @@ router.get('/', async (req, res, next) => {
 module.exports = router;
 
 async function fetchDataFromPostgres() {
-  log.debug('[v3BaseSCPoolAPY] Fetching data from postgres..');
+  log.debug('[v3BaseSCPoolAPYHistory] Fetching data from postgres..');
   const queryResult = await postgresClient.query(
-    'select ts, pool_id, collateral_type, collateral_value, debt, hourly_issuance, hourly_pnl, cumulative_pnl, cumulative_issuance, rewards_usd, hourly_pnl_pct, hourly_rewards_pct, apr_24h, apy_24h, apr_7d, apy_7d, apr_28d, apy_28d, apr_24h_pnl, apy_24h_pnl, apr_7d_pnl, apy_7d_pnl, apr_28d_pnl, apy_28d_pnl, apr_24h_rewards, apy_24h_rewards, apr_7d_rewards, apy_7d_rewards, apr_28d_rewards, apy_28d_rewards from base_mainnet.fct_core_apr WHERE pool_id = 1 order by ts desc limit 1;',
+    'select ts, pool_id, collateral_type, collateral_value, debt, hourly_issuance, hourly_pnl, cumulative_pnl, cumulative_issuance, rewards_usd, hourly_pnl_pct, hourly_rewards_pct, apr_24h, apy_24h, apr_7d, apy_7d, apr_28d, apy_28d, apr_24h_pnl, apy_24h_pnl, apr_7d_pnl, apy_7d_pnl, apr_28d_pnl, apy_28d_pnl, apr_24h_rewards, apy_24h_rewards, apr_7d_rewards, apy_7d_rewards, apr_28d_rewards, apy_28d_rewards from base_mainnet.fct_core_apr WHERE pool_id = 1 order by ts desc limit 100000;',
   );
 
-  const {
-    ts,
-    pool_id,
-    collateral_type,
-    collateral_value,
-    debt,
-    hourly_issuance,
-    hourly_pnl,
-    cumulative_pnl,
-    cumulative_issuance,
-    rewards_usd,
-    hourly_pnl_pct,
-    hourly_rewards_pct,
-    apr_24h,
-    apy_24h,
-    apr_7d,
-    apy_7d,
-    apr_28d,
-    apy_28d,
-    apr_24h_pnl,
-    apy_24h_pnl,
-    apr_7d_pnl,
-    apy_7d_pnl,
-    apr_28d_pnl,
-    apy_28d_pnl,
-    apr_24h_rewards,
-    apy_24h_rewards,
-    apr_7d_rewards,
-    apy_7d_rewards,
-    apr_28d_rewards,
-    apy_28d_rewards,
-  } = queryResult.rows[0];
+  const dailyResults = queryResult.rows
+    .filter((_, index) => index % 24 === 0)
+    .map(
+      ({
+        ts,
+        pool_id,
+        collateral_type,
+        collateral_value,
+        debt,
+        hourly_issuance,
+        hourly_pnl,
+        cumulative_pnl,
+        cumulative_issuance,
+        rewards_usd,
+        hourly_pnl_pct,
+        hourly_rewards_pct,
+        apr_24h,
+        apy_24h,
+        apr_7d,
+        apy_7d,
+        apr_28d,
+        apy_28d,
+        apr_24h_pnl,
+        apy_24h_pnl,
+        apr_7d_pnl,
+        apy_7d_pnl,
+        apr_28d_pnl,
+        apy_28d_pnl,
+        apr_24h_rewards,
+        apy_24h_rewards,
+        apr_7d_rewards,
+        apy_7d_rewards,
+        apr_28d_rewards,
+        apy_28d_rewards,
+      }) => {
+        const timestamp = ts;
+        const poolId = pool_id;
+        const collateralType = collateral_type;
+        const collateralValue = parseFloat(collateral_value);
+        const debtAmount = parseFloat(debt);
+        const hourlyIssuance = parseFloat(hourly_issuance);
+        const hourlyPnl = parseFloat(hourly_pnl);
+        const cumulativePnl = parseFloat(cumulative_pnl);
+        const cumulativeIssuance = parseFloat(cumulative_issuance);
+        const rewardsUSD = parseFloat(rewards_usd);
+        const hourlyPnlPct = parseFloat(hourly_pnl_pct);
+        const hourlyRewardsPct = parseFloat(hourly_rewards_pct);
+        const apr24h = parseFloat(apr_24h);
+        const apy24h = parseFloat(apy_24h);
+        const apr7d = parseFloat(apr_7d);
+        const apy7d = parseFloat(apy_7d);
+        const apr28d = parseFloat(apr_28d);
+        const apy28d = parseFloat(apy_28d);
+        const apr24hPnl = parseFloat(apr_24h_pnl);
+        const apy24hPnl = parseFloat(apy_24h_pnl);
+        const apr7dPnl = parseFloat(apr_7d_pnl);
+        const apy7dPnl = parseFloat(apy_7d_pnl);
+        const apr28dPnl = parseFloat(apr_28d_pnl);
+        const apy28dPnl = parseFloat(apy_28d_pnl);
+        const apr24hRewards = parseFloat(apr_24h_rewards);
+        const apy24hRewards = parseFloat(apy_24h_rewards);
+        const apr7dRewards = parseFloat(apr_7d_rewards);
+        const apy7dRewards = parseFloat(apy_7d_rewards);
+        const apr28dRewards = parseFloat(apr_28d_rewards);
+        const apy28dRewards = parseFloat(apy_28d_rewards);
 
-  const timestamp = ts;
-  const poolId = pool_id;
-  const collateralType = collateral_type;
-  const collateralValue = parseFloat(collateral_value);
-  const debtAmount = parseFloat(debt);
-  const hourlyIssuance = parseFloat(hourly_issuance);
-  const hourlyPnl = parseFloat(hourly_pnl);
-  const cumulativePnl = parseFloat(cumulative_pnl);
-  const cumulativeIssuance = parseFloat(cumulative_issuance);
-  const rewardsUSD = parseFloat(rewards_usd);
-  const hourlyPnlPct = parseFloat(hourly_pnl_pct);
-  const hourlyRewardsPct = parseFloat(hourly_rewards_pct);
-  const apr24h = parseFloat(apr_24h);
-  const apy24h = parseFloat(apy_24h);
-  const apr7d = parseFloat(apr_7d);
-  const apy7d = parseFloat(apy_7d);
-  const apr28d = parseFloat(apr_28d);
-  const apy28d = parseFloat(apy_28d);
-  const apr24hPnl = parseFloat(apr_24h_pnl);
-  const apy24hPnl = parseFloat(apy_24h_pnl);
-  const apr7dPnl = parseFloat(apr_7d_pnl);
-  const apy7dPnl = parseFloat(apy_7d_pnl);
-  const apr28dPnl = parseFloat(apr_28d_pnl);
-  const apy28dPnl = parseFloat(apy_28d_pnl);
-  const apr24hRewards = parseFloat(apr_24h_rewards);
-  const apy24hRewards = parseFloat(apy_24h_rewards);
-  const apr7dRewards = parseFloat(apr_7d_rewards);
-  const apy7dRewards = parseFloat(apy_7d_rewards);
-  const apr28dRewards = parseFloat(apr_28d_rewards);
-  const apy28dRewards = parseFloat(apy_28d_rewards);
+        // old code - left for backward compatiblity
+        const aprPnl = parseFloat(apr_7d_pnl);
+        const aprRewards = parseFloat(apr_7d_rewards);
+        const aprCombined = parseFloat(apr_7d);
 
-  // old code - left for backward compatiblity
-  const aprPnl = parseFloat(apr_7d_pnl);
-  const aprRewards = parseFloat(apr_7d_rewards);
-  const aprCombined = parseFloat(apr_7d);
+        return {
+          timestamp,
+          poolId,
+          collateralType,
+          collateralValue,
+          debtAmount,
+          hourlyIssuance,
+          hourlyPnl,
+          cumulativePnl,
+          cumulativeIssuance,
+          rewardsUSD,
+          hourlyPnlPct,
+          hourlyRewardsPct,
+          apr24h,
+          apy24h,
+          apr7d,
+          apy7d,
+          apr28d,
+          apy28d,
+          apr24hPnl,
+          apy24hPnl,
+          apr7dPnl,
+          apy7dPnl,
+          apr28dPnl,
+          apy28dPnl,
+          apr24hRewards,
+          apy24hRewards,
+          apr7dRewards,
+          apy7dRewards,
+          apr28dRewards,
+          apy28dRewards,
+          // old code - left for backward compatiblity
+          aprPnl,
+          aprRewards,
+          aprCombined,
+        };
+      },
+    );
 
-  const responseData = {
-    timestamp,
-    poolId,
-    collateralType,
-    collateralValue,
-    debtAmount,
-    hourlyIssuance,
-    hourlyPnl,
-    cumulativePnl,
-    cumulativeIssuance,
-    rewardsUSD,
-    hourlyPnlPct,
-    hourlyRewardsPct,
-    apr24h,
-    apy24h,
-    apr7d,
-    apy7d,
-    apr28d,
-    apy28d,
-    apr24hPnl,
-    apy24hPnl,
-    apr7dPnl,
-    apy7dPnl,
-    apr28dPnl,
-    apy28dPnl,
-    apr24hRewards,
-    apy24hRewards,
-    apr7dRewards,
-    apy7dRewards,
-    apr28dRewards,
-    apy28dRewards,
-    // old code - left for backward compatiblity
-    aprPnl,
-    aprRewards,
-    aprCombined,
-  };
-  log.debug('[v3BaseSCPoolAPY] Setting cache..');
-  await setCache(cacheKey, responseData, 60);
-  return responseData;
+  log.debug('[v3BaseSCPoolAPYHistory] Setting cache..');
+  await setCache(cacheKey, dailyResults, 60);
+  return dailyResults;
 }
