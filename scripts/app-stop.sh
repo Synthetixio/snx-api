7f55bacc3db450eb0dac76902f5e9e51b973f89f
Noisekit <noisekit@proton.me>
2024-12-06 17:49:49 +1100

    Don't remove anything without force

--- scripts/app-stop.sh
+++ scripts/app-stop.sh
@@ -3,6 +3,6 @@ home_path=/home/ubuntu
 bin_path=/usr/bin
 docker_path=$home_path/.docker/cli-plugins
 
-cd $home_path/snx-api &&
-	$docker_path/docker-compose down --volumes &&
-	$bin_path/docker system prune --all --volumes --force
+cd $home_path/snx-api
+$docker_path/docker-compose down
+$bin_path/docker system prune --all --volumes --force
