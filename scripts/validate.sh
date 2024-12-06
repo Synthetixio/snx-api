2db010ca4622bf77316bcdca5d72c76100c46c8b
Noisekit <noisekit@proton.me>
2024-12-06 15:46:44 +1100

    More timeouts

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,6 +6,7 @@ sleep 60
 status_code=$(curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:80/status)
 if [[ $status_code -ne 200 ]]; then
 	echo "App is not healthy - status code: $status_code"
+	curl -v http://localhost:80/status
 	exit 1
 else
 	echo "App is responding with status code: $status_code"
