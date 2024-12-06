f71938ae42dce6106f256fd17c12e645390d4159
Noisekit <noisekit@proton.me>
2024-12-06 18:47:41 +1100

    Better logs

--- scripts/app-stop.sh
+++ scripts/app-stop.sh
@@ -4,5 +4,9 @@ bin_path=/usr/bin
 docker_path=$home_path/.docker/cli-plugins
 
 cd $home_path/snx-api
+
+echo "docker-compose down"
 $docker_path/docker-compose down
+
+echo "docker system prune --all --volumes --force"
 $bin_path/docker system prune --all --volumes --force
