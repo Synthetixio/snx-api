98c19a94932d7861baa9b7642ed93af8a7bfa9c0
Noisekit <noisekit@proton.me>
2024-12-06 17:41:21 +1100

    Cannot remove without force

--- scripts/app-stop.sh
+++ scripts/app-stop.sh
@@ -4,5 +4,5 @@ bin_path=/usr/bin
 docker_path=$home_path/.docker/cli-plugins
 
 cd $home_path/snx-api &&
-	$docker_path/docker-compose down --volumes --rmi all &&
+	$docker_path/docker-compose down --volumes &&
 	$bin_path/docker system prune --all --volumes --force
